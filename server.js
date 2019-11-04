acac79cbe3fcc00faccf2dcee7fa5d3fdc9b155e
shaunorpen <shaunorpen@users.noreply.github.com>
2019-11-04 12:55:25 +0000

    Add catchall endpoint

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.get("*", (req, res) => {
+  res.json("API up and running!");
+});
 
 module.exports = server;
